21773115f23ccd117f280a290dd410802bf17aea
Anoop Sharma <anoopksharma22@gmail.com>
2022-01-22 23:02:46 +0530

    alternate_way_quill

--- frontend/src/components/Editor.vue
+++ frontend/src/components/Editor.vue
@@ -1,59 +1,32 @@
-<template>
-  <div class="container">
-    <QuillEditor theme="snow" :toolbar="toolbar" :modules="modules" />
-  </div>
-</template>
+<script setup>
+import { ref, reactive, onMounted } from "vue";
+import Quill from "quill";
+import "quill/dist/quill.snow.css";
 
-<script>
-// import { ref, defineComponent } from "vue";
-import { QuillEditor } from "@vueup/vue-quill";
-import BlotFormatter from "quill-blot-formatter";
-import "@vueup/vue-quill/dist/vue-quill.snow.css";
+const editor = ref(null);
+const quill = ref(null);
+const content = ref(null);
+const options = reactive({
+  modules: {
+    syntax: false,
+  },
+  placeholder: "Note here",
+  theme: "snow",
+});
 
-export default {
-  components: {
-    QuillEditor,
-  },
-  setup: () => {
-    const toolbar = [
-      [{ header: [1, 2, 3, 4, 5, 6, false] }],
-      ["bold", "italic", "underline", "strike"], // toggled buttons
-      [{ script: "sub" }, { script: "super" }], // superscript/subscript
-      [{ color: [] }, { background: [] }], // dropdown with defaults from theme
-      [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
-      ["blockquote", "code-block"],
-      [{ list: "ordered" }, { list: "bullet" }],
-      [{ align: [] }],
-      ["clean"], // remove formatting button
-      // [{ direction: "rtl" }], // text direction
-      // [{ header: 1 }, { header: 2 }], // custom button values
-      // [{ size: ["small", false, "large", "huge"] }], // custom dropdown
-      // [{ font: [] }],
-    ];
-    const modules = {
-      name: "blotFormatter",
-      module: BlotFormatter,
-      options: {
-        /* options */
-      },
-    };
-    return { modules, toolbar };
-  },
+onMounted(() => {
+  if (editor.value !== null) {
+    quill.value = new Quill(editor.value, options);
+  }
+});
+
+const setContent = () => {
+  content.value = quill.value.getContents();
+  console.log(content.value);
 };
 </script>
 
-<style>
-#app {
-  display: flex;
-  justify-content: center;
-}
-.container {
-}
-.ql-container {
-  background: rgba(250, 247, 224, 0.466)
-    radial-gradient(rgba(0, 0, 0, 0.3) 1px, transparent 1px);
-  background-size: 25px 25px;
-  height: 90vh;
-  box-shadow: 0 0 2px 0px;
-}
-</style>
+<template>
+  <div ref="editor"></div>
+  <button @click="setContent">save</button>
+</template>
