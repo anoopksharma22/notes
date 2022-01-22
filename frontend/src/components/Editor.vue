ae1de584de8bc7ba3bca6ee47d77b155baf7ed44
Anoop Sharma <anoopksharma22@gmail.com>
2022-01-22 20:19:53 +0530

    custome tool bar

--- frontend/src/components/Editor.vue
+++ frontend/src/components/Editor.vue
@@ -17,20 +17,18 @@ export default {
   setup: () => {
     const toolbar = [
       [{ header: [1, 2, 3, 4, 5, 6, false] }],
-      // [{ header: 1 }, { header: 2 }], // custom button values
       ["bold", "italic", "underline", "strike"], // toggled buttons
       [{ script: "sub" }, { script: "super" }], // superscript/subscript
       [{ color: [] }, { background: [] }], // dropdown with defaults from theme
       [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
       ["blockquote", "code-block"],
       [{ list: "ordered" }, { list: "bullet" }],
+      [{ align: [] }],
+      ["clean"], // remove formatting button
       // [{ direction: "rtl" }], // text direction
-
+      // [{ header: 1 }, { header: 2 }], // custom button values
       // [{ size: ["small", false, "large", "huge"] }], // custom dropdown
       // [{ font: [] }],
-      [{ align: [] }],
-
-      ["clean"], // remove formatting button
     ];
     const modules = {
       name: "blotFormatter",
